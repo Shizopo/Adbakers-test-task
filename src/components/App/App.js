705d201f39e729cc895fc7c955e61f5ec054b691
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2020-01-22 12:44:18 +0200

    boilerplate

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Adbakers test task</h1>
+    </div>
+  );
+}
+
+export default App;
