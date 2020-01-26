e99046ae7559a78014b24c8257cbd3b8bbd9fe43
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2020-01-26 11:39:00 +0200

    add pagination

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Users.sass";
 
-const Users = ({ users, numberOfPages, currentPage, renderUsers }) => {
+const renderUsers = arr => {
+  return arr.map(({ id, first_name, last_name, avatar }) => (
+    <li key={id.toString()} className="UsersList_User">
+      <span className="UsersList_User_FirstName">{first_name}</span>
+      <span className="UsersList_User_LastName">{last_name}</span>
+    </li>
+  ));
+};
+
+const Users = ({ users, handlePageChange }) => {
   if (!users) {
     return (
       <>
@@ -14,6 +23,10 @@ const Users = ({ users, numberOfPages, currentPage, renderUsers }) => {
     <div className="Users">
       <h1>Users page</h1>
       <ul className="UsersList">{renderUsers(users)}</ul>
+      <nav className="UsersNavigation">
+        <button onClick={() => handlePageChange(false)}>Previous</button>
+        <button onClick={() => handlePageChange(true)}>Next</button>
+      </nav>
     </div>
   );
 };
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -27,13 +27,29 @@ class UsersContainer extends React.Component {
       .catch(err => console.log("An error occured", err));
   };
 
-  renderUsers = arr => {
-    return arr.map(({ id, first_name, last_name, avatar }) => (
-      <li key={id.toString()}>
-        <span>{first_name}</span>
-        <span>{last_name}</span>
-      </li>
-    ));
+  handlePageChange = async button => {
+    let { users, currentPage, numberOfPages } = this.state;
+    let nextPage = currentPage + 1;
+    let prevPage = currentPage - 1;
+    if (button && nextPage > numberOfPages) {
+      this.setState({
+        currentPage: 1,
+      });
+    } else if (!button && prevPage <= 0) {
+      this.setState({
+        currentPage: numberOfPages,
+      });
+    } else if (!button) {
+      this.setState({
+        currentPage: prevPage,
+      });
+    } else if (button) {
+      users.length < numberOfPages
+        ? await this.fetchUsers(nextPage)
+        : this.setState({
+            currentPage: currentPage + 1,
+          });
+    }
   };
 
   render() {
@@ -42,9 +58,7 @@ class UsersContainer extends React.Component {
       <>
         <Users
           users={users[currentPage - 1]}
-          numberOfPages={numberOfPages}
-          currentPage={currentPage}
-          renderUsers={this.renderUsers}
+          handlePageChange={this.handlePageChange}
         />
       </>
     );
